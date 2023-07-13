6f1e22acba07cef9aac9410335a775652e6b5dba
rudhraa_r <111883798+rudhraa-r@users.noreply.github.com>
2023-07-13 22:51:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Hangman-OX
-An attempt to create a hangman game using HTML , CSS & JS .
-Referred various sources to familiarize with the concepts.
+# HOPSCOTCH
--- script.js
+++ script.js
@@ -1,234 +1,183 @@
-const letterContainer = document.getElementById("letter-container");
-const optionsContainer = document.getElementById("options-container");
-const userInputSection = document.getElementById("user-input-section");
-const newGameContainer = document.getElementById("new-game-container");
-const newGameButton = document.getElementById("new-game-button");
-const canvas = document.getElementById("canvas");
-const resultText = document.getElementById("result-text");
+//const places = document.querySelectorAll('empty');
 
+const  draggableElements = document.querySelectorAll('.draggable');
+const droppableElements =document.querySelectorAll('.droppable');
+const notification = document.querySelector('.notification');
+const overlay = document.querySelector('.overlay');
+const correctCountElement = document.querySelector('.correct-count .count');
+const wrongCountElement = document.querySelector('.wrong-count .count');
+const energyFillElement = document.querySelector('.energy-fill');
 
-let options = {
-    fruits: [
-        "Apple",
-        "Blueberry", "Mandarin", "Pineapple" , "Pomegranate" , "Watermelon",
-    ],
-    animals: ["Hedgehog", "Rhinoceros", "Squirrel" ,"Panther", "Walrus", "Zebra"],
-    countries: [
-        "India", "Hungary", "Kyrgyzstan","Switzerland", "Zimbabwe", "Dominica"
-    ],
-};
+const maxCount = 8;
+let correctCount = 0;
+let wrongCount = 0;
 
-//count 
-let winCount = 0;
-let count =0;
+draggableElements.forEach(elem => {
+    elem.addEventListener("dragstart",dragStart);
+});
 
-let chosenWord = "";
+droppableElements.forEach(elem =>{
+    elem.addEventListener("dragover",dragOver);
+    elem.addEventListener("drop", drop);
+    elem.addEventListener("dragenter", dragEnter);
+    elem.addEventListener("dragleave", dragLeave);
+});
 
-//Display option buttons 
-const displayOptions = () => {
-    let buttonCon = document.createElement("div");
-    for (let value in options) {
-      buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
-    }
-    optionsContainer.appendChild(buttonCon);
-  };
-
-const initializer = () => {
-    winCount=0;
-    count=0;
-
-    userInputSection.innerHTML ="";
-    optionsContainer.innerHTML="";
-    letterContainer.classList.add("hide");
-    newGameContainer.classList.add("hide");
-    letterContainer.innerHTML="";
-
-    //For creating letter buttons
-    for(let i=65 ; i<91 ; i++){
-        let button = document.createElement("button");
-        button.classList.add("letters");
-        button.innerText=String.fromCharCode(i);
-
-        button.addEventListener('click' , () =>{
-            let charArray = chosenWord.split('');
-            let dashes = document.querySelectorAll(".dashes");
-
-            if(charArray.includes(button.innerText)){
-                charArray.forEach((char , index) =>{
-                    //if character in array is same as clciked buton
-                    if(char === button.innerText){
-                        //replace dash with letter
-                        dashes[index].innerHTML=char;
-                        //increment counter
-                        winCount+= 1;
-                        if(winCount == charArray.length) {
-                            resultText.innerHTML = `<h2 class='win-msg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`;
-                            blocker();
-                        }
-                    }
-                })
-            }
-            else{
-                count+=1;
-                //for drawing man
-                drawMan(count);
-                //count == 6 , head, body , left arm , right arm , left leg , right leg
-                console.log(count);
-                if(count == 6){
-                    resultText.innerHTML = `<h2 class='lose-msg'>You Lose :( </h2><p>The word was <span>${chosenWord}</span></p>`;
-                     blocker();      
-                }
-            }
-            //disable clicked button
-            button.disabled = true;
-        })
-
-        letterContainer.append(button);
-    }
-
-    displayOptions();
-    //call to canvascreator
-    let {initialDrawing} = canvasCreator();
-    //initialDrawing would draw the frame
-    initialDrawing();
+function dragStart(event){
+    event.dataTransfer.setData("text", event.target.id);
+}
+function dragEnter(event){
+    event.target.classList.add("droppable-hover");
+}
+function dragLeave(event){
+    event.target.classList.remove("droppable-hover")
 }
 
-const canvasCreator = () => {
-    let context = canvas.getContext("2d");
-    context.beginPath();
-    context.strokeStyle = "#000";
-    context.lineWidth = 2;
-
-    //For drawing lines 
-    const drawLine = (fromX , fromY , toX , toY) => {
-        context.moveTo(fromX , fromY);
-        context.lineTo (toX, toY);
-        context.stroke();
-    };
-
-    const head = () =>{
-        context.beginPath();
-        context.arc(70, 30, 10, 0, Math.PI * 2 , true);
-        context.stroke();
-    };
-
-    const body = () => {
-        drawLine(70, 40 , 70 , 80);
-    };
-
-    const leftArm = () =>{
-        drawLine(70,50,50,70);
-    };
-
-    const rightArm = () =>{
-        drawLine(70,50,90,70);
-    };
-
-    const leftLeg = () =>{
-        drawLine(70,80,50,110);
-    };
-
-    const rightLeg = () =>{
-        drawLine(70,80,90,100);
-    };
-
-    //initial frame
-const initialDrawing = () =>{
-    //clear canvas
-    context.clearRect(0, 0 , context.canvas.width , context.canvas.height);
-    //bottom line
-    drawLine(10,130,130,130);
-    //left line
-    drawLine(10,10,10,131);
-    //top line
-    drawLine(10,10,70,10);
-    //small top line
-    drawLine(70,10,70,20);
-};
-
-return {initialDrawing,head,body,leftArm,rightArm , leftLeg,rightLeg};
-
-};
-
-const drawMan = (count) => {
-    let { head , body , leftArm , rightArm , leftLeg , rightLeg} = canvasCreator();
-    switch (count) {
-        case 1:
-            head();
-            break;
-        case 2:
-            body();
-            break;
-        case 3:
-            leftArm();
-            break;
-        case 4:
-            rightArm();
-            break;
-        case 5:
-            leftLeg();
-            break;
-        case 6:
-            rightLeg();
-            break;
-        default:
-            break;
-    }
+function dragOver(event) {
+    event.preventDefault();
 }
 
-
-newGameButton.addEventListener("click", initializer);
-window.onload = initializer;
-
-const generateWord = (optionValue) => {
-    let optionsButtons = document.querySelectorAll(".options");
-
-    optionsButtons.forEach((button) =>{
-        if(button.innerHTML === optionValue){
-            button.classList.add("active");
-        }
-        button.disabled= true;
-    });
-
-    letterContainer.classList.remove("hide");
-    userInputSection.innerText="";
-
-    let optionArray = options[optionValue];
-    //choose random word
+function drop(event) {
+    event.preventDefault();
+    const draggableElementData = event.dataTransfer.getData("text");
+    const droppableElementData = event.target.getAttribute("data-draggable-id");
+    if(draggableElementData === droppableElementData) {
+        event.target.classList.add("dropped");
+        const draggableElement = document.getElementById(draggableElementData);
+        event.target.style.backgroundColor = draggableElement.style.backgroundColor;
+        draggableElement.classList.add("dragged");
+        draggableElement.setAttribute("draggable", "false");
+        const newDiv = document.createElement("div");
+    newDiv.classList.add("square.draggableElementData");
+    event.target.insertBefore(newDiv, event.target.firstChild);
+        incrementCorrectCount();
+    const correctTiles = document.querySelectorAll('.dropped');
+    if (correctTiles.length === 8) {
+      showCongratulations();
+    }
+  } else {
+    showNotification();
+  }
+}
     
-    chosenWord = optionArray[Math.floor(Math.random()*optionArray.length)]
-    chosenWord = chosenWord.toUpperCase();
-    console.log(chosenWord); 
-
-    //replace each element with dash and display
-
-    let displayItem = chosenWord.replace(/./g , '<span class = "dashes"> -</span>');
-    userInputSection.innerHTML = displayItem;
+function showNotification() {
+    let wrong = document.createElement("span");
+    wrong.classList.add("wrong-notif");
+    notification.innerHTML = "";
+    wrong.innerHTML = `Oops, that is not right. Match the number with the correct word`;
+    notification.append(wrong);
+    notification.classList.add("show");
+    overlay.style.display = "block";
+    incrementWrongCount();
+    setTimeout(closeDiv, 2000);
 }
-
-const blocker = ()=>{
-    let optionsButtons = document.querySelectorAll(".options");
-    let letterButtons = document.querySelectorAll(".letters");
-
-    optionsButtons.forEach((button) => {
-        button.disabled =true;
-    })
-
-    letterButtons.forEach((button)=>{
-        button.disabled = true;
-    })
-    newGameContainer.classList.remove("hide");
+  
+function closeDiv() {
+    notification.classList.remove("show");
+    overlay.style.display = "none";
 }
+  
+function incrementCorrectCount() {
+    correctCount++;
+    correctCountElement.textContent = correctCount;
+    updateEnergyBar();
+    if (correctCount === 8) {
+      // All correct moves made, perform necessary actions
+      // For example, show a winning message
+      console.log("Congratulations! You've completed all correct moves.");
+    }
+  }
+  
+  function incrementWrongCount() {
+    wrongCount++;
+    wrongCountElement.textContent = wrongCount;
+    updateEnergyBar();
+  }
+  
+  function updateEnergyBar() {
+    const energyBar = document.querySelector(".energy-fill");
+    const fillPercentage = (correctCount / maxCount) * 100;
+    energyBar.style.width = `${fillPercentage}%`;
+  }
 
+  function showCongratulations() {
+    const congratulationsDiv = document.createElement("div");
+    congratulationsDiv.classList.add("congratulations");
+  
+    const message = document.createElement("div");
+    message.textContent = "Congratulations! You have arranged all tiles correctly.";
+    congratulationsDiv.appendChild(message);
+  
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button-container");
+  
+    /*const replayButton = document.createElement("button");
+    replayButton.textContent = "Replay";
+    replayButton.classList.add("play-button");
+    replayButton.addEventListener("click", replay);
+    buttonContainer.appendChild(replayButton);*/
+  
+    const nextLevelButton = document.createElement("button");
+    nextLevelButton.textContent = "Next Level";
+    nextLevelButton.classList.add("play-button");
+    nextLevelButton.addEventListener("click", nextLevel);
+    buttonContainer.appendChild(nextLevelButton);
+  
+    /*const exitButton = document.createElement("button");
+    exitButton.textContent = "Exit";
+    exitButton.classList.add("play-button");
+    exitButton.addEventListener("click", exit);
+    buttonContainer.appendChild(exitButton);*/
+  
+    congratulationsDiv.appendChild(buttonContainer);
+  
+    document.body.appendChild(congratulationsDiv);
+  }
+  
+  /*function replay() {
+    // Logic to replay the level goes here
+    location.href = "https://rudhraa-r.github.io/Pandi_Aatam-Lvl-1/";;
+  }*/
+  
+  function nextLevel() {
+    // Logic to proceed to the next level goes here
 
+  }
+  
+ /* function exit() {
+    // Logic to exit the game or go back to the main menu goes here
+    window.open="main.html";
+  }*/
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
  
+  
+  
+  
+  
+  
+  
+  
 
-
-
-
-
-
-
-
-
-
+/*function showNotification(){
+    let wrong = document.createElement("span");
+    wrong.classList.add("wrong-notif");
+    notification.style.display=true; 
+    wrong.innerHTML =`Oops , that is not right . Match the number with the correct tile`;
+    notification.append(wrong);
+    setTimeout(closeDiv,3000);
+}
+function closeDiv(){
+    let get = document.getElementById('popup');
+    get.style.display = 'none';
+   };
+*/
